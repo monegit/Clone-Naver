bf0c4a289df01e11595ebf5e0b39c79663c03f81
Jongwon Park <pjw970220@gmail.com>
2020-12-13 00:26:22 +0900

    init

---
+++ App/src/App.js
@@ -0,0 +1,18 @@
+import Search from "./Elements/Search";
+
+function App() {
+  return (
+    <div className="App" style={{ height:"110vh" }} >
+      <Search
+        // style={{
+        //   right: 0,
+        //   position: "relative",
+        //   top: "50px",
+        //   marginLeft: "100",
+        // }}
+      />
+    </div>
+  );
+}
+
+export default App;
